0342c3954b0388fef1d4ab4b99c37a952e9ac3ea
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-03 19:22:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,7 +31,8 @@ class App < Sinatra::Base
         num1 = params[:number1]
         num2 = params[:number2]
         operation = params[:operation]
-          
+        
+        case switch   
         
         
         
