6c8e072a7e7bcbf8943484b39566c3736808d7d3
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-03 19:05:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
-
-end
+  get '/reversename/:name' do 
+  
+  name = params[:name]
+  name = name.reverse
+  end 
+  
+  get '/square/:number' do 
+    num = params[:number].to_i ** 2 
+   
+  end
+end 
